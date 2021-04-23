cc71d59673d73cd845e65c0640b395ff34bd274a
Ale Fedrizzi <81780311+AleFedrizzi@users.noreply.github.com>
2021-04-23 19:25:40 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,52 +10,52 @@ int main()
 {
     setlocale(LC_ALL, "Portuguese"); // Usado para o CodeBlocks reconhecer a lingua portuguesa como idioma.
 
-    // Declaração de variáveis
+    // DeclaraÃ§Ã£o de variÃ¡veis
     int temp=0, i=0, j=0, pot=7, numero, opcao, binario;
     int resto[i], digito[i];
 
-    // Menu de interação
-    cout << "Calculadora de Conversão\n\n";
-    cout << "Escolha a opção desejada: \n\n\n";
-    cout << "Digite 1: Decimal para binário\n";
-    cout << "Digite 2: Binário para decimal\n\n";
+    // Menu de interaÃ§Ã£o
+    cout << "Calculadora de ConversÃ£o\n\n";
+    cout << "Escolha a opÃ§Ã£o desejada: \n\n\n";
+    cout << "Digite 1: Decimal para binÃ¡rio\n";
+    cout << "Digite 2: BinÃ¡rio para decimal\n\n";
     cin >> opcao;
 
-    //Decimal para binário
+    //Decimal para binÃ¡rio
     if(opcao == 1){
-        cout << "\nDigite o número: ";
+        cout << "\nDigite o nÃºmero: ";
         cin >> numero;
-        while(numero!=0){ // Laço onde occore a divisao do número por 2, enquanto esse número for diferente de 0"
-            resto[i] = numero%2; //O resto da divisão é guardado no vetor "resto" na posição "i", ao final do laço é feito um incremento na posição do vetor
+        while(numero!=0){ // LaÃ§o onde ocorre a divisÃ£o do nÃºmero por 2, enquanto esse nÃºmero for diferente de 0"
+            resto[i] = numero%2; //O resto da divisÃ£o Ã© guardado no vetor "resto" na posiÃ§Ã£o "i", ao final do laÃ§o Ã© feito um incremento na posiÃ§Ã£o do vetor
             numero/=2;
             i++;
         }
-        cout << "\nO número em binário é: ";
-        for(int j=i-1; j>=0; j--){ // Laço que imprime os valores do vetor "resto", da última posição até a primeira
-            cout << resto[j]; // Saída
+        cout << "\nO nÃºmero em binÃ¡rio Ã©: ";
+        for(int j=i-1; j>=0; j--){ // LaÃ§o que imprime os valores do vetor "resto", da Ãºltima posiÃ§Ã£o atÃ© a primeira
+            cout << resto[j]; // SaÃ­da
         }
 
-    // Binário para decimal
+    // BinÃ¡rio para decimal
     }else if(opcao == 2){
-        cout << "Digite um número em binário: \n\n";
+        cout << "Digite um nÃºmero em binÃ¡rio: \n\n";
         cin >> binario;
 
-        //Laço que separa o numero digitado em casas de um bit
+        //LaÃ§o que separa o nÃºmero digitado em casas de um bit
         for(i=0; i<8; i++){
             digito[i]=binario%10;
             binario/=10;
         }
-        /*laço que faz a multiplicação de cada digito com a potencia de 2 elevado a pot,
-        sendo que pot está icializada em 7 e recebe um decremento a cada loop*/
+        /*laÃ§o que faz a multiplicaÃ§Ã£o de cada digito com a potÃªncia de 2 elevado a pot,
+        sendo que pot estÃ¡ inicializada em 7 e recebe um decremento a cada loop*/
         for(i=7; i>=0; i--){
             digito[i]=digito[i]*pow(TAM,pot);
             pot--;
         }
-        /*Laço que soma o resultado da multiplicação de cada posição*/
+        /*LaÃ§o que soma o resultado da multiplicaÃ§Ã£o de cada posiÃ§Ã£o*/
         for(j=0; j<8; j++){
             temp=temp+digito[j];
         }
-        cout<<"\n\nO número em decimal é:" << temp; //Saída
+        cout<<"\n\nO nÃºmero em decimal Ã©:" << temp; //SaÃ­da
 
         }
 
